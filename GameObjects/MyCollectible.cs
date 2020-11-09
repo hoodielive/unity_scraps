ba881c56ee3a57792c3d25f0778268c2e9c73867
hoodielive <hoodielive@gmail.com>
2020-11-08 23:13:51 -0500

    Addendum to MyCollectables

--- GameObjects/MyCollectible.cs
+++ GameObjects/MyCollectible.cs
@@ -2,7 +2,13 @@ using System.Unity;
 
 public class MyCollectable : MonoBehaviour 
 {
-	void Start() { } 
+	public int ID; 
+	public float size; 
+
+	void Start() 
+	{
+		this.transform.localScale = new Vector3(size, size, size)
+	} 
 	void Update() { } 
 
 	void OnTriggerEnter2D(Collider c)
